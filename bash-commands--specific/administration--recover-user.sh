daf8bca96d082a2c9f8f006790ce7b3a48678df6
David Lechner <david.lechner@talsen.team>
2019-03-21 00:07:05 +0100

    attempt to fix issue #1

--- bash-commands--specific/administration--recover-user.sh
+++ bash-commands--specific/administration--recover-user.sh
@@ -12,8 +12,8 @@ VAR_PASS_OF_USER="${5}"
 
 echo -E "Recovering test user named '${VAR_NAME_OF_USER}' with password '${VAR_PASS_OF_USER}' ..."
 
-docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
-               exec -T ${VAR_NAME_OF_SERVICE}             \
-                    /bin/bash /opt/matrix-synapse/recover-matrix-user.sh "${VAR_NAME_OF_USER}" "${VAR_PASS_OF_USER}"
+docker exec --tty                  \
+            ${VAR_NAME_OF_SERVICE} \
+            /bin/bash /opt/matrix-synapse/recover-matrix-user.sh "${VAR_NAME_OF_USER}" "${VAR_PASS_OF_USER}"
 
 echo -e "Recovering test user named '${VAR_NAME_OF_USER}' with password '${VAR_PASS_OF_USER}' ... $( __done )"
