53655377f4accddc91bbf55c56b3fd95d51f53c4
David Lechner <david.lechner@talsen.team>
2019-12-07 17:29:21 +0100

    add compatibility flag to custom command

--- bash-commands--specific/setup--generate-configuration.sh
+++ bash-commands--specific/setup--generate-configuration.sh
@@ -11,7 +11,7 @@ echo -E "Generating matrix-synapse configuration ..."
 HOST_SERVICE_SERVER_MATRIX_SYNAPSE_COMMAND=generate
 HOST_SERVICE_SERVER_MATRIX_SYNAPSE_RESTART_POLICY=no
 
-docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
+docker-compose --compatibility --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
                up
 
 echo -e "Generating matrix-synapse configuration ... $( __done )"
